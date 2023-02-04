13cfa1042b905dfb6cb80287b7632e3bc9b4e19e
SAmuelX100 <samuel118.sainthilaire@gmail.com>
2023-02-04 15:55:20 -0400

    First function added

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 const botones = document.querySelectorAll('.key');
+const pantalla_entrada = document.querySelector('.pantalla .entrada');
+const pantalla_salida = document.querySelector('.pantalla .salida');
+
+let entrada = "";
+
+for (let key of botones) {
+    const value = key.dataset.key;
+
+    key.addEventListener('click', () => {
+        if (value == "borrar") {
+            entrada = "";
+            pantalla_entrada.innerHTML = "";
+            pantalla_salida.innerHTML = "";
+        } else if (value == "backspace") {
+
+        }
+
+    })
+}
