6937a93d0ed56e043da8d972200dc4449c4107a8
SAmuelX100 <samuel118.sainthilaire@gmail.com>
2023-02-05 09:16:36 -0400

    Basic functions now working

--- main.js
+++ main.js
@@ -13,8 +13,35 @@ for (let key of botones) {
             pantalla_entrada.innerHTML = "";
             pantalla_salida.innerHTML = "";
         } else if (value == "backspace") {
+            entrada = entrada.slice(0, -1);
+            pantalla_entrada.innerHTML = entrada;
+        } else if (value == "=") {
+            let resultado = eval(entrada);
 
+            pantalla_salida.innerHTML = resultado;
+        } else if (value == "parentesis") {
+            if (entrada.indexOf("(") == -1 || entrada.indexOf("(") != -1 && entrada.indexOf(")") != -1 && entrada.lastIndexOf("(") < entrada.lastIndexOf(")")) {
+                entrada += "(";
+            } else if (
+                entrada.indexOf("(") != -1 &&
+                entrada.indexOf(")") == -1 ||
+                entrada.indexOf("(") != -1 &&
+                entrada.indexOf(")") != -1 &&
+                entrada.lastIndexOf("(") > entrada.lastIndexOf(")")) {
+                entrada += ")";
+            }
+
+            pantalla_entrada.innerHTML = entrada;
+
+        } else {
+            entrada += value;
+            pantalla_entrada.innerHTML = entrada;
         }
 
     })
+}
+
+function LimpiarEntrada(entrada) {
+
+    let entrada_array = entrada.split("");
 }
